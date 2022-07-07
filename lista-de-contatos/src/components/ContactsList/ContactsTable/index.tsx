8101dd9e8a89bcf9b85919b8c765ca05ca5ac13e
Marcus <vini.mark2002@gmail.com>
2022-07-07 02:11:10 -0300

    #2 Adicionada lista com base de dados em JSON

--- lista-de-contatos/src/components/ContactsList/ContactsTable/index.tsx
+++ lista-de-contatos/src/components/ContactsList/ContactsTable/index.tsx
@@ -1,32 +1,55 @@
 import React, {Fragment} from 'react';
 import {FiEdit} from 'react-icons/fi/index.js';
-import {BsTrash} from 'react-icons/bs/index.js';
+import {FaRegTrashAlt} from 'react-icons/fa/index.js';
 
 import './styles.css';
+
+import Data from './test.json';
+
+function AppendNewContacts() {
+    if (Data !== null) {
+        const newLines = Data.map(element => {
+            return(
+                <tr className="contact-row">
+                    <td className="column">{element.Nome}</td>
+                    <td className="column">{element.Sobrenome}</td>
+                    <td className="column">{element.Telefone}</td>
+                    <td className="column">{element.Email}</td>
+                    <td className="column">
+                        <div className="icons">
+                            <FiEdit className="edit-icon"/>
+                            <FaRegTrashAlt className="delete-icon"/>
+                        </div>
+                    </td>
+                </tr>
+            );
+        });
+        return(newLines);
+    } else return(<tr><td>Não há contatos</td></tr>);
+}
+
+function TableConstruction() {
+    return (
+        <Fragment>
+            <table className="contacts-table">
+                <tr className="contact-row">
+                    <th className="column">Nome</th>
+                    <th className="column">Sobrenome</th>
+                    <th className="column">Telefone</th>
+                    <th className="column">E-mail</th>
+                    <th className="column"></th>
+                </tr>
+                
+                {AppendNewContacts()}
+            </table>
+        </Fragment>
+    );
+}
 
 export function ContactsTable() {
     return (
         <Fragment>
-            <table className="contacts-table">
-                <td>
-                    <tr className="column-title">Nome</tr>
-                </td>
-                <td>
-                    <tr className="column-title">Sobrenome</tr>
-                </td>
-                <td>
-                    <tr className="column-title">Telefone</tr>
-                </td>
-                <td>
-                    <tr className="column-title">E-mail</tr>
-                </td>
-                <td>
-                    <tr>
-                        <FiEdit/>
-                        <BsTrash/>
-                    </tr>
-                </td>
-            </table>
+            {TableConstruction()}
         </Fragment>
     );
 }
