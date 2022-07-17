a2341eb738492a1d146ed6cfa658db0970328a19
João Pedro Morbeck <jpmorbeck45@gmail.com>
2022-07-16 21:57:12 -0300

    #4 atualização de dados pronto

--- lista-de-contatos/back/routes.js
+++ lista-de-contatos/back/routes.js
@@ -16,19 +16,47 @@ contatoRoute.post('/create', async (req, res) => {
 
 contatoRoute.delete('/delete/:id', async (req, res) => {
     const id = req.params.id;
-    const delContato = await Models.contatoModel.findOne({_id: id}).catch((err) => res.status(500));
+    const delContato = await Models.contatoModel.findOne({_id: id}).catch((err) => {
+        res.status(400).json({message: "ID em formato incorreto"});
+    });
 
     if(!delContato){
-        res.status(424).json({message: "Usuário não encontrado"});
+        res.status(404).json({message: "Contato não encontrado"});
         return;
     }
 
     try {
         await Models.contatoModel.deleteOne({_id: id});
-        res.status(200).json({message: "Usuário deletado com sucesso"});
+        res.status(200).json({message: "Contato deletado com sucesso"});
     } catch (err) {
         res.status(500).json({error: err});
     }
+});
+
+contatoRoute.patch('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const {nome, sobrenome, email, telefone} = req.body
+
+    const contato = {
+        nome,
+        sobrenome,
+        email,
+        telefone
+    };
+
+    try {
+        const updatedContato = await Models.contatoModel.updateOne({_id: id}, contato)
+
+        if(updatedContato.matchedCount === 0){
+            res.status(404).json({message:"Contato não foi encontrado"});
+            return;
+        }
+
+        res.status(200).json(contato);
+    } catch (err) {
+        res.status(500).json({error: err});
+    }
+
 });
 
 module.exports = {contatoRoute};
